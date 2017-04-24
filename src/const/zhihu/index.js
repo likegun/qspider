7a333b65f27a98af03b08d17ccf952f6b96e065f
wrq <likegun94@gmail.com>
2017-04-24 09:39:30 +0800

    删除知乎的一切

--- src/const/zhihu/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-module.exports = {
-  'FOLLOW_API_URL_FORMAT': 'https://www.zhihu.com/api/v4/members/%s/followers?offset=%d&limit=%d',
-  'HOME_PAGE_URL_FORMAT': 'https://www.zhihu.com/people/%s/followers'
-};
